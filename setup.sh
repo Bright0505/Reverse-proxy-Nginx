173d9a586e84e0c0d10aac12cc9c5624a56add5a
Bright0505 <bright.su72@gmail.com>
2022-06-21 08:53:53 +0800

    faet: add nginx config test

--- setup.sh
+++ setup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 PS3='Please enter your choice: '
-options=("reload" "restart" "CLI" "Quit")
+options=("test config" "reload" "restart" "CLI" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
+        "test config")
+            docker exec -it $(docker-compose ps -q web) nginx -t
+            ;;
         "reload")
             docker exec -it $(docker-compose ps -q web) nginx -s reload
             ;;
         "restart")
-	    docker-compose restart
+            docker-compose restart
             ;;
         "CLI")
-            docker exec -it $(docker-compose ps -q web) /bin/sh 
+            docker exec -it $(docker-compose ps -q web) /bin/sh
+            break
             ;;
         "Quit")
             break
